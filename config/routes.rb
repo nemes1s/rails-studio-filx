095743f33971abc95dfc53f1f9802dd64626136e
Dmytro Zadorozhnyi <dmitriyzadorozniy@gmail.com>
2022-01-13 14:18:08 +0100

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # verb "url" => "name_of_controller#name_of_action"
-
+  root "movies#index"
   get "movies" => "movies#index"
+  get "movies/:id" => "movies#show", as: "movie"
+  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
+  patch "movies/:id" => "movies#update", as: "update_movie"
 end
